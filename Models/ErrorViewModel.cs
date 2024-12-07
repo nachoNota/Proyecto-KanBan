4a8477e281b8df9d3beddad9bb6a098969648aeb
nachoNota <nachonota13@gmail.com>
2024-12-07 20:55:14 -0300

    creo un nuevo proyecto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_KanBan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
